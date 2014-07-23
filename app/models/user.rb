dd2a31cda5c99decfd8423f6096765ea26b8d439
App Academy Student <student@appacademy.io>
2014-07-23 14:07:19 -0700

    Very basic user auth implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,36 @@
 class User < ActiveRecord::Base
-  validates :email, :session_token, presence: true
+  attr_reader :password
+  
+  validates :email, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+  
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+  
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+  
+  def password=(secret)
+    self.password_digest = BCrypt::Password.create(secret)
+  end
+  
+  def is_password?(secret)
+    BCrypt::Password.new(self.password_digest).is_password?(secret)
+  end
+  
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+  
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
