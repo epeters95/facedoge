75f5ec0cbac56b007dadd6f35d69d1a5547410b3
App Academy Student <student@appacademy.io>
2014-07-31 18:07:45 -0700

    Sidebar work

--- app/assets/javascripts/views/sidebar.js
+++ app/assets/javascripts/views/sidebar.js
@@ -1,4 +1,6 @@
-Facedoge.Views.Sidebar = Backbone.CompositeView.extend({  
+Facedoge.Views.Sidebar = Backbone.CompositeView.extend({
+  template: JST["sidebar"],
+  
   initialize: function() {
     this.currentUser;
     
@@ -6,6 +8,7 @@ Facedoge.Views.Sidebar = Backbone.CompositeView.extend({
     Facedoge.allUsers.fetch({ success: function() {
        that.currentUser = Facedoge.currentUser();
        that.listenTo(that.currentUser, 'sync', that.render);
+       that.listenTo(that.currentUser.friendships(), 'add remove', that.render);
     }});
   },
   
@@ -27,19 +30,44 @@ Facedoge.Views.Sidebar = Backbone.CompositeView.extend({
     });
   },
   
+  addRequestViews: function() {
+    var requests = this.currentUser.friendships().where({
+      in_friend_id: this.currentUser.id
+    });
+    var that = this;
+    _(requests).each(function(request) {
+      var user = Facedoge.allUsers.get(request.get("out_friend_id"));
+      if (!that.currentUser.isFriendsWith(user)) {
+        var exists = false;
+        _(that.subviews('.request-items')).each(function(view) {
+          if (view.model.id === request.get("out_friend_id")) { exists = true }
+        });
+        if (!exists) {
+          user.fetch();
+          var requestView = new Facedoge.Views.UserShow({
+            model: user
+          });
+          that.addSubview('.request-items', requestView);
+        }
+      }
+    })
+  },
+  
   render: function() {
     this.$el.html('');
     if (this.currentUser) {
       this.addFriendViews();
+      this.addRequestViews();
     }
-    
-    this.$el.append($('<li class="item"></li>'));
-    
-    var view = this;
-    _(this.subviews('.friend-items')).each(function (subview) {
-      var content = subview.render().$el;
-      content.insertAfter('.item');
-    });
+    this.$el.html(this.template());
+    this.attachSubviews();
+    // this.$el.append($('<li class="item"></li>'));
+    //
+    // var view = this;
+    // _(this.subviews('.friend-items')).each(function (subview) {
+    //   var content = subview.render().$el;
+    //   content.insertAfter('.item');
+    // });
     return this;
   }
 });
